6bea1b7888783787996dd30fcbdb43c1501c954b
satishsahu <satish.sahu@gmail.com>
2020-05-09 20:07:29 +0530

    # WARNING: head commit changed in the meantime
    first commit

---
+++ src/test/java/com/techm/banking/user/registration/UserRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techm.banking.user.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
